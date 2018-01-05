b443e01d7c662502d911e9e496bcd86c5a669971
Vinge1718 <vinge1718@gmail.com>
2018-01-05 18:57:56 +0300

    add the innitial display files

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App{
+    public static void main(String[] args){
+        ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
+        setPort(port);
+
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+    }
+}
