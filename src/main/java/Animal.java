44a2b8d3be9ea4da6fb24a05b192783e62ccb8b1
Vinge1718 <vinge1718@gmail.com>
2018-04-25 21:08:17 +0300

    add the class for the animal class

---
+++ src/main/java/Animal.java
@@ -0,0 +1,21 @@
+public class Animal{
+    private String name;
+
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+        if(!(otherAnimal instanceof Animal)){
+            return false;
+        }else{
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+        }
+    }
+}
