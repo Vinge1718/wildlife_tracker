15fd28f4fe5194f5dbf4da8652395eec927f49fd
Vinge1718 <vinge1718@gmail.com>
2018-05-16 17:17:43 +0300

    change the animal constructor parameters

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
+import java.util.Arrays;
 
 public class AnimalTest {
 
@@ -25,4 +26,49 @@ public class AnimalTest {
         Animal secondAnimal = new Animal("Lion");
         assertTrue(testAnimal.equals(secondAnimal));
     }
+
+    @Test
+    public void save_returnsTrueIfDescriptionIsSame(){
+        Animal testAnimal = new Animal("Lion");
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
+    }
+
+    @Test
+    public void save_InstantiatesObjectWithId_int(){
+        Animal testAnimal = new Animal("Lion");
+        testAnimal.save();
+        Animal anotherAnimal = Animal.all().get(0);
+        assertEquals(testAnimal.getId(), anotherAnimal.getId());
+    }
+    @Test
+    public void all_returnsAllInstancesOfAnimal_list(){
+        Animal firstAnimal = new Animal("Lion");
+        firstAnimal.save();
+        Animal secondAnimal = new Animal("Rhino");
+        secondAnimal.save();
+        assertEquals(true, Animal.all().get(0).equals(firstAnimal));
+        assertEquals(true, Animal.all().get(1).equals(secondAnimal));
+    }
+
+    @Test
+    public void find_returnsAnimalObjectBasedOnId_object(){
+        Animal firstAnimal = new Animal("Lion");
+        firstAnimal.save();
+        Animal secondAnimal = new Animal("Boar");
+        secondAnimal.save();
+        assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
+    }
+
+
+    // @Test
+    // public void animals_sightingCanReturnAllAnimalsAssociated_list(){
+    //    Animal testAnimal = new Animal("Lion");
+    //    Sighting firstSighting = new Sighting("John", "North Forest", testAnimal.getId());
+    //    firstSighting.save();
+    //    Sighting secondSighting = new Sighting("Jane", "SE Forest", testAnimal.getId());
+    //    secondSighting.save();
+    //    Object[] sightings = new Object[] {firstSighting, secondSighting};
+    //    assertTrue(testAnimal.getSightings().containsAll(Arrays.asList(sightings)));
+    // }
 }
